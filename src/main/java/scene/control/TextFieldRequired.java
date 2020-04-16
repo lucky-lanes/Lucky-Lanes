e1360e38e6798411ccaf46019e3a8c2731603ba3
christelgw27 <60200292+christelgw27@users.noreply.github.com>
2020-04-16 12:28:41 -0500

    Update Comments/Ordering TextFieldRequired.java
    I updated each method's comment to ensure it had a proper javadoc formatting and included a description as well as descriptions for the @param and @return tags.
    
    Moved the isValid(String text) method up to just below where the isValid() method is to keep them together.

--- src/main/java/scene/control/TextFieldRequired.java
+++ src/main/java/scene/control/TextFieldRequired.java
@@ -17,7 +17,7 @@ public class TextFieldRequired extends TextField {
     private boolean isRequired;
 
     /**
-     *
+     * Constructor
      */
     public TextFieldRequired() {
         super("");
@@ -27,22 +27,29 @@ public class TextFieldRequired extends TextField {
     }
 
     /**
-     * @param r
+     * Setter method for isRequired
+     *
+     * @param r Boolean value to set the TextField as required or not
      */
     public void setRequired(boolean r) {
         this.isRequired = r;
     }
 
     /**
-     * @return
+     * Getter method for variable isRequired
+     *
+     * @return Boolean value of if the TextField is currently set to required
      */
     public boolean isRequired() {
         return this.isRequired;
     }
 
     /**
-     * @param regex
-     * @param validationPrompt
+     * Sets the validation prompt for the TextField as well as the regular expression which the entered text
+     * be tested against
+     *
+     * @param regex String containing a regular expression
+     * @param validationPrompt String of the message that will appear when the mouse hovers over the TextField
      */
     public void setValidation(String regex, String validationPrompt) {
         this.validationRegex = regex;
@@ -71,14 +78,21 @@ public class TextFieldRequired extends TextField {
     }
 
     /**
-     * @return
+     * If the field is required (isRequired set to True), checks for if the entered text matches
+     * the currently set regular expression
+     *
+     * @return Boolean value that is true if isRequired is set to true and the result of isValid() is true
      */
     public boolean validate() {
         return isRequired && isValid();
     }
 
     /**
-     * @return
+     * Checks the text that is currently entered in the TextField to see if it matches the currently set
+     * regular expression. Will affect the style of the TextField depending upon the returned value
+     *
+     * @return Boolean value that is true if the field is empty, true if the entered text matches the currently
+     * set regular expression, and false if there is text and it doesn't match
      */
     public boolean isValid() {
         if (isRequired) {
@@ -102,12 +116,22 @@ public class TextFieldRequired extends TextField {
 
         return true;
     }
+    
+    /**
+     * Tests the input parameter String to see if it matches the currently set regular expression pattern
+     *
+     * @param text String of text to be tested against the currently set regular expression
+     * @return Boolean value for if it matches the set regular expression
+     */
+    public boolean isValid(String text) {
+        return text.matches(validationRegex);
+    }
 
 
     /**
      * Used to validate a field without affecting its style
      *
-     * @return
+     * @return Boolean value of if the text in the text field matches the currently set regular expression
      */
     public boolean isValidNoEffect() {
         if (isRequired) {
@@ -127,13 +151,5 @@ public class TextFieldRequired extends TextField {
         }
 
         return true;
-    }
-
-    /**
-     * @param text
-     * @return
-     */
-    public boolean isValid(String text) {
-        return text.matches(validationRegex);
     }
 }
