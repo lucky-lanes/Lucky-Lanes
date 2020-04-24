936831fb1c884284970e0909adc4f4582b609987
christelgw27 <60200292+christelgw27@users.noreply.github.com>
2020-04-24 11:51:49 -0500

    Comment updates SearchController.java
    Added javadoc comments to almost all variables. Additionally, added javadoc comments to all methods of the class. I did not add javadoc comments to the methods that are inside other methods.
    
    Removed some unnecessary comments and some unnecessary blank lines

--- src/main/java/controllers/SearchController.java
+++ src/main/java/controllers/SearchController.java
@@ -35,32 +35,66 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 /**
- * FXML Controller class
+ * FXML Controller class for Search.fxml
+ * <p>
+ * This is the controller for the Search Athletes page that comes up when you click on the Search Athletes button on the admin screen
  *
  * @author nicolenelson
  */
 public class SearchController implements Initializable {
-    // variables from the fxml file
+    /**
+     * Table that holds the information on all athletes
+     */
     @FXML
     TableView table;
+    /**
+     * Textfield under the search section for entering a name to search by
+     */
     @FXML
     TextField txtName;
+    /**
+     * Textfield under the search section for entering an address to search by
+     */
     @FXML
     TextField txtAddress;
+    /**
+     * Textfield under the search section for entering a state to search by
+     */
     @FXML
     TextField txtState;
+    /**
+     * Textfield under the search section for entering a city to search by
+     */
     @FXML
     TextField txtCity;
+    /**
+     * Textfield under the search section for entering a school to search by
+     */
     @FXML
     TextField txtSchool;
+    /**
+     * Textfield under the search section for entering a sport to search by
+     */
     @FXML
     TextField txtSport;
+    /**
+     * Textfield under the search section for entering a zip code to search by
+     */
     @FXML
     TextField txtZip;
+    /**
+     * Textfield under the search section for entering an ID to search by
+     */
     @FXML
     TextField txtID;
+    /**
+     * Submit/Refresh button under the search section on the left hand side of the screen
+     */
     @FXML
     Button buttonSubmit;
+    /**
+     * Spinning wheel icon that appears on the screen when the submit/refresh button is clicked
+     */
     @FXML
     ProgressIndicator progressIndicator;
 
@@ -70,12 +104,28 @@ public class SearchController implements Initializable {
     private ResultSet rs;
     private Report report;
 
-    private Stage stage;                //Used to allow the user to go back to the previous scene. Otherwise not used.
-
-    private Scene preScene;             //The previous screens scene while using the back button.
-    private double preMinHeight;        //The previous minimum screens height.
-    private double preMinWidth;         //The previous minimum screens width.
-    private String preTitle;            //The previous screens title.
+    /**
+     * The window.
+     * <p>
+     * Note from prev. semester: Used to allow the user to go back to the previous scene. Otherwise not used.
+     */
+    private Stage stage;
+    /**
+     * The previous screen's scene while using the back button
+     */
+    private Scene preScene;
+    /**
+     * The previous screen's minimum height
+     */
+    private double preMinHeight;  
+    /**
+     * The previous screen's minimum width
+     */
+    private double preMinWidth;
+    /**
+     * The previous screen's title
+     */
+    private String preTitle;
 
     /**
      * Initializes the controller class.
@@ -99,6 +149,10 @@ public class SearchController implements Initializable {
     }
 
     /**
+     * Method to print reports on all athletes
+     * <p>
+     * Ran when the Save all Reports button is clicked
+     *
      * @param e
      */
     @FXML
@@ -129,7 +183,7 @@ public class SearchController implements Initializable {
                 @Override
                 public Void call() throws Exception {
                     progressIndicator.setVisible(false);
-                    System.out.println("Creating Objets to save");
+                    System.out.println("Creating Objects to save");
                     p.createObjects();
                     p.toDocs();
 
@@ -169,6 +223,10 @@ public class SearchController implements Initializable {
     }
 
     /**
+     * Method to print reports on a selected user.
+     * <p>
+     * Ran when the Save Selected Reports button is clicked
+     *
      * @param e
      */
     @FXML
@@ -241,8 +299,9 @@ public class SearchController implements Initializable {
     }
 
     /**
-     * Does it need a parameter?
-     * Action Events can only be created by event handlers tied to ui elements
+     * Ran when the Submit/Refresh button is clicked
+     * <p>
+     * Note from prev Semester: Does it need a parameter? Action Events can only be created by event handlers tied to ui elements
      *
      * @param e
      */
@@ -278,7 +337,7 @@ public class SearchController implements Initializable {
             @Override
             public Void call() throws Exception {
                 //BEHOLD
-                progressIndicator.setVisible(true);//FUCK YES!!!!
+                progressIndicator.setVisible(true);
 
                 //Platform.runlater is used to update an UI control inside a different thread.
                 Database.connect();
@@ -318,7 +377,6 @@ public class SearchController implements Initializable {
 
                                 //System.out.println("Column ["+i+"] ");
                             }
-
 
                             // add data to the observable list
                             while (rs.next()) {
@@ -376,9 +434,7 @@ public class SearchController implements Initializable {
                                     };
 
                                     return cell;
-                                }
-
-                                ;
+                                };
                             };
 
                             editCol.setCellFactory(cellFactory);
@@ -387,7 +443,6 @@ public class SearchController implements Initializable {
                         } catch (SQLException ex) {
                             //Logger.getLogger(SearchController.class.getName()).log(Level.SEVERE, null, ex);
                         }
-
                     }
                 });
 
@@ -419,6 +474,12 @@ public class SearchController implements Initializable {
         exec.execute(databaseQuery);
     }
 
+    /**
+     * This is the method ran for an athlete when the Open button on their line is clicked
+     * It will open up the athlete's menu
+     *
+     * @param id Id of an athlete
+     */
     private void editAthlete(String id) {
 
         String fxml = "/main/resources/view/AthleteMenu.fxml";
@@ -461,17 +522,18 @@ public class SearchController implements Initializable {
         }
     }
 
-    @FXML
-
     /**
      * Adds the previous scene into the object to allow the user to go back to it with the back button.
+     *
      * @param pre The previous scene.
      */
+    @FXML
     protected void setPreScene(Scene pre) {
         preScene = pre;
     }
 
     /**
+     * Puts things in the current window. Changes the scene to the current one.
      * Sets the stage object with data so the back button can change the scene to go back.
      *
      * @param stage The window.
@@ -502,8 +564,3 @@ public class SearchController implements Initializable {
         stage.setTitle(preTitle);
     }
 }
-
-
-
-
-
