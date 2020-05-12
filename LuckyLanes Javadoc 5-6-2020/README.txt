ba392909ce411ccd75268e8ffc87260a783b71a7
christelgw27 <christelgw27@uww.edu>
2020-05-12 13:13:14 -0500

    Minor Javadoc comment updates. Generated final Javadoc

--- LuckyLanes Javadoc 5-6-2020/README.txt
+++
@@ -1,3 +0,0 @@
-This verison of the Javadoc was generated on 5/6/2020
-
-To open up the javadoc, open the index.html file in your web browser
--- src/main/formObjects/IBSSN.java
+++ src/main/formObjects/IBSSN.java
@@ -11,57 +11,57 @@ import main.java.Database;
 public class IBSSN {
 
     /**
-     * User-entered target accuracy score, values 0.0 to 10.0
+     * Target accuracy score, values 0.0 to 10.0
      */
     private double targetAccuracy = 0;
-    
+
     /**
-     * User-entered target accuracy score, values 0 to 100%
+     * Pocket percentage, values 0 to 100%
      */
     private double pocketPercentage = 0;
-    
+
     /**
-     * User-entered single pin spare conversion score, values 0 to 100%
+     * Single pin spare conversion score, values 0 to 100%
      */
     private double singlePinSpareConv = 0;
-    
+
     /**
-     * User-entered multiple pin spare conversion score, values 0 to 100%
+     * Multiple pin spare conversion score, values 0 to 100%
      */
     private double multiplePinSpareConv = 0;
-    
+
     /**
-     * User-entered number of throws to pocket, values 1 to 10
+     * Average of number of throws to hit the pocket for the first time, values 1 to 10
      */
     private int avgThrowsToPocket = 0;
-    
+
     /**
-     * User-entered number of throws to pocket after adjustment, values 1 to 10
+     * Average number of throws to hit the pocket after adjustment, values 1 to 10
      */
     private int avgThrowsToPocketAdjusted = 0;
-    
+
     /**
-     * User-entered entry angle of ball, values 0 to 10.0 
+     * Entry angle of ball, values 0 to 10.0
      */
     private double entryAngle = 0;
-    
+
     /**
-     * User-entered ball speed consistency, values 0.0 to 1
+     * Ball speed consistency, values 0.0 to 1
      */
     private double ballSpeedConsistency = 0;
-    
+
     /**
-     * User-entered ball speed at release, values 0 and above
+     * Ball speed at release, values 0 and above
      */
     private double ballSpeed = 0;
-    
+
     /**
      * Gender of the bowler. 0 = female, 1 = male
      */
     private int gender = 0;
-    
+
     /**
-     * User-entered rev rate at release, values 0 and above
+     * Rev rate at release, values 0 and above
      */
     private double revRate = 0;
 
@@ -73,21 +73,21 @@ public class IBSSN {
     /**
      * Constructor method for the IBSSN object
      *
-     * @param targetAccuracy
-     * @param pocketPercentage
-     * @param singlePinSpareConv
-     * @param multiplePinSpareConv
-     * @param avgThrowsToPocket
-     * @param avgThrowsToPocketAdjusted
-     * @param entryAngle
-     * @param ballSpeedConsistency
-     * @param ballSpeed
+     * @param targetAccuracy Target accuracy score, values 0.0 to 10.0
+     * @param pocketPercentage Target accuracy score, values 0 to 100%
+     * @param singlePinSpareConv Single pin spare conversion score, values 0 to 100%
+     * @param multiplePinSpareConv Multiple pin spare conversion score, values 0 to 100%
+     * @param avgThrowsToPocket Average of number of throws to hit the pocket for the first time, values 1 to 10
+     * @param avgThrowsToPocketAdjusted Average number of throws to hit the pocket after adjustment, values 1 to 10
+     * @param entryAngle Entry angle of ball, values 0 to 10.0
+     * @param ballSpeedConsistency Ball speed consistency, values 0.0 to 1
+     * @param ballSpeed Ball speed at release, values 0 and above
      * @param gender Gender of the bowler. 0 = female, 1 = male
-     * @param revRate
+     * @param revRate Rev rate at release, values 0 and above
      */
-    public IBSSN(double targetAccuracy, double pocketPercentage, double singlePinSpareConv, double multiplePinSpareConv, int avgThrowsToPocket, 
-    		int avgThrowsToPocketAdjusted, double entryAngle, double ballSpeedConsistency, double ballSpeed, int gender, double revRate){
-    	
+    public IBSSN(double targetAccuracy, double pocketPercentage, double singlePinSpareConv, double multiplePinSpareConv, int avgThrowsToPocket,
+                 int avgThrowsToPocketAdjusted, double entryAngle, double ballSpeedConsistency, double ballSpeed, int gender, double revRate){
+
         this.targetAccuracy = targetAccuracy;
         this.pocketPercentage = pocketPercentage;
         this.singlePinSpareConv = singlePinSpareConv;
@@ -99,7 +99,7 @@ public class IBSSN {
         this.ballSpeed = ballSpeed;
         this.gender = gender;
         this.revRate = revRate;
-        
+
         //calculate total points. This method sets the total points based off the info put into all the other vars
         setTotalPoints();
     }
@@ -118,7 +118,7 @@ public class IBSSN {
             String sql;
             sql = "INSERT INTO IBSSN VALUES ("
                     + "null, " + targetAccuracy + ", " + pocketPercentage + ", " + singlePinSpareConv +
-                    ", " + multiplePinSpareConv + ", " + avgThrowsToPocket + ", " + avgThrowsToPocketAdjusted + 
+                    ", " + multiplePinSpareConv + ", " + avgThrowsToPocket + ", " + avgThrowsToPocketAdjusted +
                     ", " + entryAngle + "," + ballSpeedConsistency +", " + ballSpeed + ", " + gender + ", " + revRate + ");";
 
             Database.executeUpdate(sql);
@@ -131,7 +131,7 @@ public class IBSSN {
                     ", singlePinSpareConv = " + singlePinSpareConv +
                     ", multiplePinSpareConv = " + multiplePinSpareConv +
                     ", avgThrowsToPocket = " + avgThrowsToPocket +
-                    ", avgThrowsToPocketAdjusted = " + avgThrowsToPocketAdjusted + 
+                    ", avgThrowsToPocketAdjusted = " + avgThrowsToPocketAdjusted +
                     ", entryAngle = " + entryAngle +
                     ", ballSpeedConsistency = " + ballSpeedConsistency +
                     ", ballSpeed = " + ballSpeed +
@@ -149,7 +149,7 @@ public class IBSSN {
      * @return An integer between 1-10 of how many points they earned
      */
     public int calculateTargetAccuracyPoints(){
-    	//find which section it fits in and assign points
+        //find which section it fits in and assign points
         int earnedPoints = 0;
 
         if(targetAccuracy < 1.8){
@@ -183,7 +183,7 @@ public class IBSSN {
      * @return An integer between 1-10 of how many points they earned
      */
     public int calculatePocketPercentagePoints(){
-    	int pointsEarned = 1;
+        int pointsEarned = 1;
         int lowerBound = 0;
         int upperBound = 10;
         for(int i = 0; i < 10; i++){
@@ -207,7 +207,7 @@ public class IBSSN {
      * @return An integer between 1-10 of how many points they earned
      */
     public int calculateSinglePinSparePoints(){
-    	//determine what percentage range they fit in and assign points
+        //determine what percentage range they fit in and assign points
         int pointsEarned = 1;
         int lowerBound = 0;
         int upperBound = 55;
@@ -231,7 +231,7 @@ public class IBSSN {
      * @return An integer between 1-10 of how many points they earned
      */
     public int calculateMultiPinSparePoints(){
-    	//determine what percentage range they fit in and assign points
+        //determine what percentage range they fit in and assign points
         int pointsEarned = 1;
         int lowerBound = 0;
         int upperBound = 50;
@@ -294,7 +294,7 @@ public class IBSSN {
      * @return An integer between 1-10 of how many points they earned
      */
     public int calculateEntryAnglePoints(){
-    	int pointsEarned;
+        int pointsEarned;
 
         if(entryAngle < 2.0){
             pointsEarned = 1;
@@ -327,7 +327,7 @@ public class IBSSN {
      * @return An integer between 1-10 of how many points they earned
      */
     public int calculateBallSpeedConsistPoints(){
-    	//find which range it falls in and assign points
+        //find which range it falls in and assign points
         int totalPoints = 0;
         if(ballSpeedConsistency >= 0.97){
             totalPoints = 1;
@@ -362,7 +362,7 @@ public class IBSSN {
     public int calculateBallSpeedReleasePoints(){
         //determine which speed section this falls into
         double speedRanges[][] = {{15.3, 15.6, 16.0, 16.4, 16.8, 17.1, 17.5}, //women's range
-                                  {16.3, 16.8, 17.4, 17.8, 18.4, 18.8, 19.0}};//men's range
+                {16.3, 16.8, 17.4, 17.8, 18.4, 18.8, 19.0}};//men's range
         int earnedPoints = 0;
         if(ballSpeed < speedRanges[gender][0]){
             earnedPoints = 3;
@@ -419,9 +419,9 @@ public class IBSSN {
      */
     public void setTotalPoints(){
         for(int i = 0; i < 11; i++) {
-        	totalPoints[i] = 0;
+            totalPoints[i] = 0;
         }
-    	
+
         totalPoints[0] += calculateTargetAccuracyPoints();
         totalPoints[1] += calculatePocketPercentagePoints();
         totalPoints[2] += calculateSinglePinSparePoints();
@@ -432,17 +432,22 @@ public class IBSSN {
         totalPoints[7] += calculateBallSpeedConsistPoints();
         totalPoints[8] += calculateBallSpeedReleasePoints();
         totalPoints[9] += calculateRevRateReleasePoints();
-        
+
         for(int i = 0; i < 10; i++) {
-        	totalPoints[10] += totalPoints[i];
+            totalPoints[10] += totalPoints[i];
         }
 
     }
-    
+
+    /**
+     * Getter for the total points earned over all of the IBSSN
+     *
+     * @return Array of values for all sections of the IBSSN score and the final summed up score
+     */
     public int[] getTotalPoints() {
-    	return totalPoints;
+        return totalPoints;
     }
-    
+
     /**
      * Creates an html table representation of the IBSSN object
      *
@@ -455,34 +460,34 @@ public class IBSSN {
         String html = "</br></br></br><table><tr><th>IBSSN</th><th></th></tr>";
         html += rowStart + "Target Accuracy Value" + rowMid + this.targetAccuracy + rowEnd
                 + rowStart + "Target Accuracy Score" + rowMid + calculateTargetAccuracyPoints() + "/10" + rowEnd
-                
+
                 + rowStart + "Pocket Percentage Value" + rowMid + this.pocketPercentage + rowEnd
                 + rowStart + "Pocket Percentage Score" + rowMid + calculatePocketPercentagePoints() + "/10" + rowEnd
-                
+
                 + rowStart + "Single Pin Spare Percentage" + rowMid + this.singlePinSpareConv + "%" + rowEnd
                 + rowStart + "Single Pin Spare Score" + rowMid + calculateSinglePinSparePoints() + "/10" + rowEnd
-                
+
                 + rowStart + "Multiple Pin Spare Percentage" + rowMid + this.multiplePinSpareConv + "%" + rowEnd
                 + rowStart + "Multiple Pin Spare Score" + rowMid + calculateMultiPinSparePoints() + "/10" + rowEnd
-                
+
                 + rowStart + "Average Number of Throws to Pocket" + rowMid + this.avgThrowsToPocket + rowEnd
                 + rowStart + "Pocket Percentage Score" + rowMid + calculateNoAdjustVersatilityPoints() + "/10" + rowEnd
-                
+
                 + rowStart + "Average Number of Throws to Pocket After Adjustment" + rowMid + this.avgThrowsToPocketAdjusted + rowEnd
                 + rowStart + "Pocket Percentage Score" + rowMid + calculateLaneAdjustVersatilityPoints() + "/10" + rowEnd
-                
+
                 + rowStart + "Ball Speed Consistency Value" + rowMid + this.ballSpeedConsistency + rowEnd
                 + rowStart + "Ball Speed Consistency Score" + rowMid + calculateBallSpeedConsistPoints() + "/10" + rowEnd
-                
+
                 + rowStart + "Entry Angle Value" + rowMid + this.entryAngle + rowEnd
                 + rowStart + "Entry Angle Score" + rowMid + calculateEntryAnglePoints() + "/10" + rowEnd
-                
+
                 + rowStart + "Ball Speed at Release Value" + rowMid + this.ballSpeed + rowEnd
                 + rowStart + "Ball Speed Score" + rowMid + calculateBallSpeedReleasePoints() + "/10" + rowEnd
-                
+
                 + rowStart + "Rev Rate Value" + rowMid + this.revRate + rowEnd
                 + rowStart + "Rev Rate Score" + rowMid + calculateRevRateReleasePoints() + "/10" + rowEnd
-                
+
                 + "</table>";
         return html;
     }
@@ -493,13 +498,13 @@ public class IBSSN {
      * @return A string containing all of the information of the IBSSN test. Each field is separated by a "|" character
      */
     public String toPDF() {
-    	setTotalPoints();
-        String pdf = "IBSSN|" + this.targetAccuracy + "|" + this.totalPoints[0] + "|" + this.pocketPercentage + "|" + this.totalPoints[1] + "|" + 
-        		+ this.singlePinSpareConv + "|" + this.totalPoints[2] + "|" + this.multiplePinSpareConv + "|" + this.totalPoints[3] + "|" 
-        		+ this.avgThrowsToPocket + "|" + this.totalPoints[4] + "|" + this.avgThrowsToPocketAdjusted + "|" + this.totalPoints[5] + "|"
-        		+ this.ballSpeedConsistency + "|" + this.totalPoints[6] +  "|" + this.entryAngle + "|" + this.totalPoints[7] + "|" 
-        		+ this.ballSpeed + "|" + this.totalPoints[8] + "|" + this.revRate + "|" + this.totalPoints[9] + "|" + this.totalPoints[10];
-        
+        setTotalPoints();
+        String pdf = "IBSSN|" + this.targetAccuracy + "|" + this.totalPoints[0] + "|" + this.pocketPercentage + "|" + this.totalPoints[1] + "|" +
+                + this.singlePinSpareConv + "|" + this.totalPoints[2] + "|" + this.multiplePinSpareConv + "|" + this.totalPoints[3] + "|"
+                + this.avgThrowsToPocket + "|" + this.totalPoints[4] + "|" + this.avgThrowsToPocketAdjusted + "|" + this.totalPoints[5] + "|"
+                + this.ballSpeedConsistency + "|" + this.totalPoints[6] +  "|" + this.entryAngle + "|" + this.totalPoints[7] + "|"
+                + this.ballSpeed + "|" + this.totalPoints[8] + "|" + this.revRate + "|" + this.totalPoints[9] + "|" + this.totalPoints[10];
+
         return pdf;
     }
 }
